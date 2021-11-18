ced78f24ccf43f7cf7e652bafd7f8ff2e9edf8b7
VMarianaVelasco <velasco.mariana250398@gmail.com>
2021-11-18 14:05:24 -0600

    Cambio y subida del chat

---
+++ README.md
@@ -0,0 +1,2 @@
+# chat3cv51VMVM.github.io
+Ejemplo de un chat y acceso a Firebase (Firestore con Google)
---
+++ js/init.js
@@ -0,0 +1,11 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyA87FDxN2GOfJsap3yQ_44bCvj97T6knXw",
+  authDomain: "fbauten-4c38e.firebaseapp.com",
+  projectId: "fbauten-4c38e",
+  storageBucket: "fbauten-4c38e.appspot.com",
+  messagingSenderId: "269385826204",
+  appId: "1:269385826204:web:6e4ad153f4b0ff6fe69259"
+    });
